2624ad9a7acb99e5c7f9dbecc779767e8b7fc380
Yehya-ya <yahya.yaser@scantitan.com>
2021-11-29 19:55:56 +0300

    register page

--- app/src/main/java/com/example/taskmanagerandroid/MainActivity.java
+++ app/src/main/java/com/example/taskmanagerandroid/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.taskmanagerandroid;
+ package com.example.taskmanagerandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
+
