6c9862b63952970c07cd920f8bc94a9ecee3f99b
Zakaria_Ramo <zakaria.rommo@gmail.com>
2021-12-01 03:04:30 +0300

    finished_except_missing_base_url

--- app/src/main/java/com/example/taskmanagerandroid/Route.java
+++ app/src/main/java/com/example/taskmanagerandroid/Route.java
@@ -3,4 +3,25 @@ package com.example.taskmanagerandroid;
 public class Route {
     public static final String BASE_URL = "https://tasks-manager-y.herokuapp.com/api";
     public static final String REGISTER = BASE_URL + "/register";
+    public static final String LOGIN = BASE_URL + "/login";
+    public static final String LOGOUT = BASE_URL + "/logout";
+    public static final String PROJECT_ALL = BASE_URL + "/";
+    public static final String PROJECT_SHOW = BASE_URL + "/";
+    public static final String PROJECT_CREATE = BASE_URL + "/";
+    public static final String UPDATE_DELETE = BASE_URL + "/";
+    public static final String UPDATE_ADD_MEMBER = BASE_URL + "/add-member";
+    public static final String UPDATE_REMOVE_MEMBER = BASE_URL + "/";
+    public static final String USER_ALL = BASE_URL + "/";
+    public static final String USER_SHOW = BASE_URL + "/";
+    public static final String USER_UPDATE = BASE_URL + "/";
+    public static final String USER_DELETE = BASE_URL + "/";
+    public static final String CATEGORY_ALL = BASE_URL + "/";
+    public static final String CATEGORY_SHOW = BASE_URL + "/";
+    public static final String CATEGORY_UPDATE = BASE_URL + "/";
+    public static final String CATEGORY_DELETE = BASE_URL + "/";
+    public static final String TASK_ALL = BASE_URL + "/";
+    public static final String TASK_SHOW = BASE_URL + "/";
+    public static final String TASK_UPDATE = BASE_URL + "/";
+    public static final String TASK_DELETE = BASE_URL + "/";
+
 }
